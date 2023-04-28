b872482fe6e23bb75d87c50dd0481789a921a98a
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-04-28 18:06:48 -0300

    feat: prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CredentialsModule } from './credentials/credentials.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, CredentialsModule],
+  imports: [UsersModule, CredentialsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
